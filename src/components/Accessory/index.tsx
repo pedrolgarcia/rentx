10ff9e9588cb17050fca74780311c737d5b25a61
Pedro Lopes <pedrolopesgrc@gmail.com>
2021-11-09 00:41:52 -0300

    feat: finish car details, button component

---
+++ src/components/Accessory/index.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { SvgProps } from "react-native-svg";
+
+import { Container, Name } from "./styles";
+
+interface AccessoryProps {
+  name: string;
+  icon: React.FC<SvgProps>;
+}
+
+function Accessory({ name, icon: Icon }: AccessoryProps) {
+  return (
+    <Container>
+      <Icon width={32} height={32} />
+      <Name>{name}</Name>
+    </Container>
+  );
+}
+
+export default Accessory;
--- src/screens/CarDetails/index.tsx
+++ src/screens/CarDetails/index.tsx
@@ -1,9 +1,33 @@
 import React from "react";
 
-import { CarImages, Container, Header } from "./styles";
+import {
+  CarImages,
+  Container,
+  Header,
+  Content,
+  Details,
+  Description,
+  Brand,
+  Name,
+  Rent,
+  Period,
+  Price,
+  Accessories,
+  About,
+  Footer,
+} from "./styles";
 
 import BackButton from "../../components/BackButton";
 import ImageSlider from "../../components/ImageSlider";
+import Accessory from "../../components/Accessory";
+
+import speedSvg from "../../assets/speed.svg";
+import accelerationSvg from "../../assets/acceleration.svg";
+import forceSvg from "../../assets/force.svg";
+import gasolineSvg from "../../assets/gasoline.svg";
+import exchangeSvg from "../../assets/exchange.svg";
+import peopleSvg from "../../assets/people.svg";
+import Button from "../../components/Button";
 
 function CarDetails() {
   return (
@@ -19,6 +43,39 @@ function CarDetails() {
           ]}
         />
       </CarImages>
+
+      <Content>
+        <Details>
+          <Description>
+            <Brand>Lamborghini</Brand>
+            <Name>Huracan</Name>
+          </Description>
+
+          <Rent>
+            <Period>Ao dia</Period>
+            <Price>R$ 580</Price>
+          </Rent>
+        </Details>
+
+        <Accessories>
+          <Accessory name="380Km/h" icon={speedSvg} />
+          <Accessory name="3.2s" icon={accelerationSvg} />
+          <Accessory name="800 HP" icon={forceSvg} />
+          <Accessory name="Gasolina" icon={gasolineSvg} />
+          <Accessory name="Auto" icon={exchangeSvg} />
+          <Accessory name="2 pessoas" icon={peopleSvg} />
+        </Accessories>
+
+        <About>
+          Este é uim automóvel desportivo. Surgiu do lendário touro de lide
+          indultado na praça Real Maestranza de Sevilla. É um belíssimo carro
+          para quem gosta de acelerar.
+        </About>
+      </Content>
+
+      <Footer>
+        <Button title="Confirmar" />
+      </Footer>
     </Container>
   );
 }
