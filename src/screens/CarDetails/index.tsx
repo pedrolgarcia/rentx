3181ac95af2302ac2782b48e0810c24a1d4055db
Pedro Lopes <pedrolopesgrc@gmail.com>
2021-11-08 23:59:15 -0300

    feat: ImageSlider indexes and image

--- src/screens/CarDetails/index.tsx
+++ src/screens/CarDetails/index.tsx
@@ -1,8 +1,9 @@
 import React from "react";
 
-import { Container, Header } from "./styles";
+import { CarImages, Container, Header } from "./styles";
 
 import BackButton from "../../components/BackButton";
+import ImageSlider from "../../components/ImageSlider";
 
 function CarDetails() {
   return (
@@ -10,6 +11,14 @@ function CarDetails() {
       <Header>
         <BackButton onPress={() => {}} />
       </Header>
+
+      <CarImages>
+        <ImageSlider
+          imagesUrl={[
+            "https://www.pngkit.com/png/full/237-2375888_porsche-panamera-s.png",
+          ]}
+        />
+      </CarImages>
     </Container>
   );
 }
