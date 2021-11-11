2794162f5f517cd9e0308e1dcd2265d49bd3d350
Pedro Lopes <pedrolopesgrc@gmail.com>
2021-11-11 01:24:23 -0300

    feat: scheduling header and footer

---
+++ src/screens/Scheduling/index.tsx
@@ -0,0 +1,62 @@
+import React from "react";
+import { StatusBar } from "react-native";
+import { useTheme } from "styled-components";
+
+import {
+  Container,
+  Header,
+  Title,
+  RentalPeriod,
+  DateInfo,
+  DateTitle,
+  DateValue,
+  Content,
+  Footer,
+} from "./styles";
+
+import BackButton from "../../components/BackButton";
+
+import ArrowSvg from "../../assets/arrow.svg";
+import Button from "../../components/Button";
+
+function Scheduling() {
+  const theme = useTheme();
+
+  return (
+    <Container>
+      <StatusBar
+        barStyle="light-content"
+        translucent
+        backgroundColor="transparent"
+      />
+      <Header>
+        <BackButton onPress={() => {}} color={theme.colors.shape} />
+        <Title>
+          Escolha uma{"\n"}data de início e{"\n"}fim do aluguel
+        </Title>
+
+        <RentalPeriod>
+          <DateInfo>
+            <DateTitle>DE</DateTitle>
+            <DateValue selected={false}>18/06/2021</DateValue>
+          </DateInfo>
+
+          <ArrowSvg />
+
+          <DateInfo>
+            <DateTitle>ATÉ</DateTitle>
+            <DateValue selected={false}>18/06/2021</DateValue>
+          </DateInfo>
+        </RentalPeriod>
+      </Header>
+
+      <Content></Content>
+
+      <Footer>
+        <Button title="Confirmar" />
+      </Footer>
+    </Container>
+  );
+}
+
+export default Scheduling;
