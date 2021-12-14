6b2952815e03ce7cce3ce4f0e3cd534dd8724db2
Pedro Lopes <pedrolopesgrc@gmail.com>
2021-12-13 23:01:07 -0300

    feat: scheduling complete page

---
+++ src/components/ConfirmButton/index.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { RectButtonProps } from "react-native-gesture-handler";
+
+import { Container, Title } from "./styles";
+
+interface ConfirmButtonProps extends RectButtonProps {
+  title: string;
+}
+
+function ConfirmButton({ title, ...rest }: ConfirmButtonProps) {
+  return (
+    <Container {...rest}>
+      <Title>{title}</Title>
+    </Container>
+  );
+}
+
+export default ConfirmButton;
