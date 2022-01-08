5c548a5b0f56527dc872768ed605c9cc2e448a06
Pedro Lopes <pedrolopesgrc@gmail.com>
2022-01-08 19:23:44 -0300

    feat: splash screen + animation

--- src/screens/Splash/index.tsx
+++ src/screens/Splash/index.tsx
@@ -1,9 +1,57 @@
-import React from "react";
+import React, { useEffect } from "react";
+import Animated, {
+  useSharedValue,
+  useAnimatedStyle,
+  interpolate,
+  withTiming,
+  Extrapolate,
+} from "react-native-reanimated";
 
 import { Container } from "./styles";
 
+import BrandSvg from "../../assets/brand.svg";
+import LogoSvg from "../../assets/logo.svg";
+
 function Splash() {
-  return <Container></Container>;
+  const splashAnimation = useSharedValue(0);
+
+  const brandStyle = useAnimatedStyle(() => {
+    return {
+      opacity: interpolate(
+        splashAnimation.value,
+        [0, 25, 50],
+        [1, 0.3, 0],
+        Extrapolate.CLAMP
+      ),
+    };
+  });
+
+  const logoStyle = useAnimatedStyle(() => {
+    return {
+      opacity: interpolate(
+        splashAnimation.value,
+        [0, 25, 50],
+        [0, 0.3, 1],
+        Extrapolate.CLAMP
+      ),
+    };
+  });
+
+  useEffect(() => {
+    splashAnimation.value = withTiming(50, { duration: 1000 });
+  }, []);
+
+  return (
+    <Container>
+      <Animated.View style={brandStyle}>
+        <BrandSvg width={80} height={50} />
+      </Animated.View>
+
+      <Animated.View style={logoStyle}>
+        <LogoSvg width={180} height={20} />
+      </Animated.View>
+    </Container>
+  );
 }
 
 export default Splash;
