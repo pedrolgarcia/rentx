83377e38aeceb0153eb47423c71e7a3effaed0ed
Pedro Lopes <pedrolopesgrc@gmail.com>
2022-01-08 22:10:31 -0300

    feat: redirect after splash animation ends

--- src/screens/Splash/index.tsx
+++ src/screens/Splash/index.tsx
@@ -5,7 +5,9 @@ import Animated, {
   interpolate,
   withTiming,
   Extrapolate,
+  runOnJS,
 } from "react-native-reanimated";
+import { useNavigation } from "@react-navigation/native";
 
 import { Container } from "./styles";
 
@@ -13,41 +15,60 @@ import BrandSvg from "../../assets/brand.svg";
 import LogoSvg from "../../assets/logo.svg";
 
 function Splash() {
+  const navigation = useNavigation();
+
   const splashAnimation = useSharedValue(0);
 
   const brandStyle = useAnimatedStyle(() => {
     return {
-      opacity: interpolate(
-        splashAnimation.value,
-        [0, 25, 50],
-        [1, 0.3, 0],
-        Extrapolate.CLAMP
-      ),
+      opacity: interpolate(splashAnimation.value, [0, 50], [1, 0]),
+      transform: [
+        {
+          translateX: interpolate(
+            splashAnimation.value,
+            [0, 50],
+            [0, -50],
+            Extrapolate.CLAMP
+          ),
+        },
+      ],
     };
   });
 
   const logoStyle = useAnimatedStyle(() => {
     return {
-      opacity: interpolate(
-        splashAnimation.value,
-        [0, 25, 50],
-        [0, 0.3, 1],
-        Extrapolate.CLAMP
-      ),
+      opacity: interpolate(splashAnimation.value, [0, 25, 50], [0, 0.3, 1]),
+      transform: [
+        {
+          translateX: interpolate(
+            splashAnimation.value,
+            [0, 50],
+            [-50, 0],
+            Extrapolate.CLAMP
+          ),
+        },
+      ],
     };
   });
 
+  function startApp() {
+    navigation.navigate("Home");
+  }
+
   useEffect(() => {
-    splashAnimation.value = withTiming(50, { duration: 1000 });
+    splashAnimation.value = withTiming(50, { duration: 1000 }, () => {
+      "worklet";
+      runOnJS(startApp)();
+    });
   }, []);
 
   return (
     <Container>
-      <Animated.View style={brandStyle}>
+      <Animated.View style={[brandStyle, { position: "absolute" }]}>
         <BrandSvg width={80} height={50} />
       </Animated.View>
 
-      <Animated.View style={logoStyle}>
+      <Animated.View style={[logoStyle, { position: "absolute" }]}>
         <LogoSvg width={180} height={20} />
       </Animated.View>
     </Container>
