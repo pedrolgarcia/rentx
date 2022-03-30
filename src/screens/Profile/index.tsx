136cd1792cf38939146e3c7213d03c4c9e278908
Pedro Lopes <pedrolopesgrc@gmail.com>
2022-03-30 02:06:29 -0300

    feat: profile page header

--- src/screens/Profile/index.tsx
+++ src/screens/Profile/index.tsx
@@ -1,20 +1,86 @@
-import { ReactNode } from 'react';
+import React, { useState } from "react";
+import { useTheme } from "styled-components";
+import { useNavigation } from "@react-navigation/native";
+import { Feather } from "@expo/vector-icons";
 
-import { Text } from 'react-native';
+import BackButton from "../../components/BackButton";
 
-import { Container } from './styles';
+import {
+  Container,
+  Header,
+  HeaderTop,
+  HeaderTitle,
+  LogoutButton,
+  PhotoContainer,
+  Photo,
+  PhotoButton,
+  Content,
+  Options,
+  Option,
+  OptionTitle,
+} from "./styles";
 
-interface ProfileProps {
-  children: ReactNode;
-}
+function Profile() {
+  const theme = useTheme();
+  const navigation = useNavigation();
 
-function Profile({ children }: ProfileProps) {
+  const [option, setOption] = useState<"dataEdit" | "passwordEdit">("dataEdit");
+
+  function handleBack() {
+    navigation.goBack();
+  }
+
+  function handleSignOut() {}
+
+  function handleOptionChange(optionSelected: "dataEdit" | "passwordEdit") {
+    setOption(optionSelected);
+  }
+
   return (
     <Container>
-      <Text>Profile</Text>
-      {children}
+      <Header>
+        <HeaderTop>
+          <BackButton color={theme.colors.shape} onPress={handleBack} />
+
+          <HeaderTitle>Editar perfil</HeaderTitle>
+
+          <LogoutButton onPress={handleSignOut}>
+            <Feather name="power" size={24} color={theme.colors.shape} />
+          </LogoutButton>
+        </HeaderTop>
+
+        <PhotoContainer>
+          <Photo
+            source={{
+              uri: "https://avatars.githubusercontent.com/u/29722492?v=4",
+            }}
+          />
+          <PhotoButton onPress={() => {}}>
+            <Feather name="camera" size={24} color={theme.colors.shape} />
+          </PhotoButton>
+        </PhotoContainer>
+      </Header>
+
+      <Content>
+        <Options>
+          <Option
+            active={option === "dataEdit"}
+            onPress={() => handleOptionChange("dataEdit")}
+          >
+            <OptionTitle active={option === "dataEdit"}>Dados</OptionTitle>
+          </Option>
+          <Option
+            active={option === "passwordEdit"}
+            onPress={() => handleOptionChange("passwordEdit")}
+          >
+            <OptionTitle active={option === "passwordEdit"}>
+              Trocar senha
+            </OptionTitle>
+          </Option>
+        </Options>
+      </Content>
     </Container>
   );
-};
+}
 
 export default Profile;
